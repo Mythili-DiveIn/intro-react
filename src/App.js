af2c6a2ee345fd026cb4caf5739e32be9ee23cd5
mythili <mythili@gmail.com>
2021-09-22 12:50:25 +0530

    made necessary implementation for task tracker

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTaskForm from './components/addTaskForm'
+//import Counter from './components/counter'
+import { useState } from 'react'
+import { Alert } from 'react-bootstrap';
 
-function App() {
+const App= () => {
+  const [showAddTask,setshowAddTask] = useState(false);
+  const [tasks, setTasks] = useState (
+    [
+
+        {
+            id:1,
+            text: 'Requirements',
+            duration: '1hr 45m'
+        },
+        {
+            id:2,
+            text: 'Define',
+            duration: '1hr'
+        },
+        {
+            id:3,
+            text: 'Design',
+            duration: '1hr 30m'
+        },
+        {
+          id:4,
+          text: 'Develop',
+          duration: '1hr 30m'
+      },
+      {
+        id:5,
+        text: 'Deploy',
+        duration: '1hr 30m'
+    }
+    ]
+)
+const addTask = (task)=> {
+  const id = Math.floor(Math.random()*10000) + 1;
+  console.log(id);
+  const newTask = {id,...task}
+  setTasks([...tasks,newTask])
+  setshowAddTask(!showAddTask)
+
+
+}
+
+const onDeleteTask = (id) => {
+  console.log('Trying to delete')
+  setTasks(tasks.filter((task)=> task.id !== id)
+  )
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+     <Header showAddTask = {()=>setshowAddTask(!showAddTask)} title='Sample Program for Task Tracker'/> 
+     {showAddTask && <AddTaskForm onAdd = {addTask}/>}
+     {tasks.length>0 ? <Tasks tasks= {tasks} onDelete={onDeleteTask}/>: 
+     <Alert className='noTaskAlert' variant="danger" >No specific tasks available</Alert>}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import '../src/index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import Counter from '../src/components/counter'
+//import AddNewItem from '../src/components/addNewItem';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
